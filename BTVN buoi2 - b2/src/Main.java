e931a29e597a0880b028df34678827d55ab2de84
Quanle-1112 <lecongquan.dtan@gmail.com>
2025-07-16 09:13:20 +0700

    btvn buoi2

---
+++ BTVN buoi2 - b2/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        String ten = "Le Cong Quan";
+        int tuoi = 19;
+        String lop = "2024KTPM01";
+        double GPA = 3.3;
+
+        System.out.print("Ten: " + ten + " - " + "Tuoi: " + tuoi + " - " + "Lop: " + lop + " - " + "GPA: " + GPA);
+
+
+    }
+}
