a235475db2b9f073cb614fce27df95848ca50418
Quanle-1112 <lecongquan.dtan@gmail.com>
2025-07-26 09:34:15 +0700

    Buoi3

---
+++ Buoi3/BTTL/BTTL b4/src/Main.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static int fibonacci(int n){
+        if(n <= 2) return 1;
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap so nguyen n: ");
+        int n = sc.nextInt();
+        System.out.println("Số fibonacci thứ n là: " + fibonacci(n));
+    }
+}
---
+++ Buoi3/BTVN/BTVN b2/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static boolean isPalindrome(String str) {
+        String reverse = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse = reverse + str.charAt(i);
+        }
+        return str.equalsIgnoreCase(reverse);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập 1 chuỗi ký tự bất kỳ: ");
+        String str = sc.nextLine();
+        if(isPalindrome(str)){
+            System.out.println(str.toUpperCase());
+            System.out.println("Chuỗi đối xứng");
+        }
+        else{
+            System.out.println(str.toLowerCase());
+            System.out.println("Chuỗi không đối xứng!");
+        }
+    }
+}
